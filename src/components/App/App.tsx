a8656b519bba7f789f58b48dc0cd186578fee5eb
Olena Ivashchenko <olenaivaschenko@ukr.net>
2025-07-14 10:53:36 +0300

    fixed export-import Movie

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,7 +4,7 @@ import SearchBar from "../SearchBar/SearchBar";
 import { fetchMovies } from "../../services/movieService";
 import { useEffect, useState } from "react";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
-import type Movie from "../../types/movie";
+import type {Movie} from "../../types/movie";
 import MovieGrid from "../MovieGrid/MovieGrid";
 import MovieModal from "../MovieModal/MovieModal";
 import Loader from "../Loader/Loader";
